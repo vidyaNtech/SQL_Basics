78f07dd1e2217e7dac51e63f2452abd56ce3aa83
Vidya N <vidyavidya12543@gmail.com>
2026-08-02 23:17:46 +0530

    "Add SELECT statement notes, examples, and practice questions"

---
+++ 01_SELECT/examples.sql
@@ -0,0 +1,19 @@
+-- Select all columns
+
+SELECT *
+FROM Students;
+
+-- Select one column
+
+SELECT Name
+FROM Students;
+
+-- Select multiple columns
+
+SELECT Name, Age
+FROM Students;
+
+-- Select Name and City
+
+SELECT Name, City
+FROM Students;
