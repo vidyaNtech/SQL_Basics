f256cebe4502ab7ddb6c902a4970c46afd6789dd
Vidya N <vidyavidya12543@gmail.com>
2026-08-03 23:22:19 +0530

    "Add WHERE clause notes, examples, practice questions, and solutions"

---
+++ 02_WHERE/solutions.sql
@@ -0,0 +1,77 @@
+-- Question 1
+
+SELECT *
+FROM Students
+WHERE Age > 20;
+
+-------------------------------------------------
+
+-- Question 2
+
+SELECT *
+FROM Students
+WHERE City = 'Bangalore';
+
+-------------------------------------------------
+
+-- Question 3
+
+SELECT *
+FROM Students
+WHERE Age < 22;
+
+-------------------------------------------------
+
+-- Question 4
+
+SELECT Name
+FROM Students
+WHERE City = 'Mysore';
+
+-------------------------------------------------
+
+-- Question 5
+
+SELECT *
+FROM Students
+WHERE StudentID > 2;
+
+-------------------------------------------------
+
+-- Question 6
+
+SELECT *
+FROM Students
+WHERE Age = 19;
+
+-------------------------------------------------
+
+-- Question 7
+
+SELECT *
+FROM Students
+WHERE City = 'Hubli';
+
+-------------------------------------------------
+
+-- Question 8
+
+SELECT *
+FROM Students
+WHERE Age <> 21;
+
+-------------------------------------------------
+
+-- Question 9
+
+SELECT Name, City
+FROM Students
+WHERE Age > 20;
+
+-------------------------------------------------
+
+-- Question 10
+
+SELECT *
+FROM Students
+WHERE City <> 'Shimoga';
