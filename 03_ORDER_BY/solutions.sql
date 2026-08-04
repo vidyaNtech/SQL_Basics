2a36d5e1ce5352e39c239a7e9741175be9e98fec
Vidya N <vidyavidya12543@gmail.com>
2026-08-04 22:49:30 +0530

    "Add ORDER BY clause notes, examples, practice questions, solutions, and interview questions"

---
+++ 03_ORDER_BY/solutions.sql
@@ -0,0 +1,77 @@
+-- Question 1
+
+SELECT *
+FROM Students
+ORDER BY Age;
+
+-------------------------------------------------
+
+-- Question 2
+
+SELECT *
+FROM Students
+ORDER BY Age DESC;
+
+-------------------------------------------------
+
+-- Question 3
+
+SELECT *
+FROM Students
+ORDER BY Name;
+
+-------------------------------------------------
+
+-- Question 4
+
+SELECT Name, City
+FROM Students
+ORDER BY City;
+
+-------------------------------------------------
+
+-- Question 5
+
+SELECT StudentID
+FROM Students
+ORDER BY StudentID DESC;
+
+-------------------------------------------------
+
+-- Question 6
+
+SELECT *
+FROM Students
+ORDER BY City, Age;
+
+-------------------------------------------------
+
+-- Question 7
+
+SELECT Name, Age
+FROM Students
+ORDER BY Age DESC;
+
+-------------------------------------------------
+
+-- Question 8
+
+SELECT *
+FROM Students
+ORDER BY Name DESC;
+
+-------------------------------------------------
+
+-- Question 9
+
+SELECT StudentID, Name
+FROM Students
+ORDER BY StudentID;
+
+-------------------------------------------------
+
+-- Question 10
+
+SELECT *
+FROM Students
+ORDER BY City ASC, Name ASC;
